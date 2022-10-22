0014dd947d2909e36d08015b2feb158c3e705ae4
natureblox <97788642+natureblox@users.noreply.github.com>
2022-10-22 09:35:58 +0000

    add gotty

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -6,3 +6,12 @@ wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" -
 cp -r tailscale tailscaled /render/
 
 mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+
+
+
+ENV GOTTY_BINARY https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_386.tar.gz
+
+RUN wget $GOTTY_BINARY -O gotty.tar.gz && \
+    tar -xzf gotty.tar.gz -C /usr/local/bin/ && \
+    rm gotty.tar.gz && \
+    chmod +x /usr/local/bin/gotty
