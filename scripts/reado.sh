1604465301130bd1e584590a29a30161a71916ee
natureblox <97788642+natureblox@users.noreply.github.com>
2022-11-06 21:24:31 +0000

    start with a dash like console

--- scripts/reado.sh
+++ scripts/reado.sh
@@ -1,3 +1,9 @@
 echo "export LANG=zh_CN.UTF-8"  >> ~/.bashrc
 echo "alias sbw='s -b w3m' "  >> ~/.bashrc
+echo "curl cn.getnews.tech"   >> ~/.bashrc
+echo "mdp -d sample.md "   >> ~/.bashrc
+echo "curl hkkr.in"   >> ~/.bashrc
+
+
+echo "https://www.medium.com/feed/@elife" >> /root/.newsboat/urls
 echo $1 >> /root/.newsboat/urls
