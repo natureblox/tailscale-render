ab9cb4dabf9f0f64246840c1fe9d278ce898f7f5
natureblox <97788642+natureblox@users.noreply.github.com>
2023-10-07 13:32:30 +0800

    hkkr not work

--- scripts/reado.sh
+++ scripts/reado.sh
@@ -2,8 +2,8 @@ echo "export LANG=zh_CN.UTF-8"  >> ~/.bashrc
 echo "alias sbw='s -b w3m' "  >> ~/.bashrc
 echo "curl cn.getnews.tech"   >> ~/.bashrc
 echo "mdp -d sample.md "   >> ~/.bashrc
-echo "curl hkkr.in"   >> ~/.bashrc
-
+#echo "curl hkkr.in"   >> ~/.bashrc
+echo "curl neuters.de"   >> ~/.bashrc
 
 echo "https://www.medium.com/feed/@elife" >> /root/.newsboat/urls
 echo $1 >> /root/.newsboat/urls
