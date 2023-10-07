145ce95dd741cdc8dd076d6da98cbcf0ef5e7e67
natureblox <97788642+natureblox@users.noreply.github.com>
2023-10-07 12:32:13 +0800

    exit node for tailscale and http proxy

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 PID=$!
 
 ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
+until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node --advertise-routes="$ADVERTISE_ROUTES"; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
